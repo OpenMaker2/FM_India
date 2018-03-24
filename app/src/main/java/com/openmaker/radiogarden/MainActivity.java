521c1d3720b78131aa463a239b228228fe028859
OpenMaker <openmaker1@gmail.com>
2018-03-24 13:50:05 +0300

    3 radio stations as demo. working

---
+++ app/src/main/java/com/openmaker/radiogarden/MainActivity.java
@@ -0,0 +1,72 @@
+package com.openmaker.radiogarden;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+    WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        myWebView = (WebView) findViewById(R.id.radiogarden);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setDomStorageEnabled(true);
+        myWebView.getSettings().setDatabaseEnabled(true);
+        myWebView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        myWebView.getSettings().setAppCacheEnabled(true);
+
+        ToggleButton toggle = (ToggleButton) findViewById(R.id.radio1switch);
+        ToggleButton toggle2 = (ToggleButton) findViewById(R.id.radio2button);
+        ToggleButton toggle3 = (ToggleButton) findViewById(R.id.radio3button);
+
+
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                    myWebView.loadUrl("http://50.7.71.27:9699/;stream.mp3");
+                } else {
+                    // The toggle is disabled
+
+                    myWebView.loadUrl("about:blank");
+
+                }
+            }
+        });
+
+        toggle2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                    myWebView.loadUrl("http://s2.voscast.com:10616/;stream1493622076708/1");
+                } else {
+                    // The toggle is disabled
+                    myWebView.loadUrl("about:blank");
+
+                }
+            }
+        });
+        toggle3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                    myWebView.loadUrl("http://192.240.102.198:14349/stream?type=http&nocache=2");
+                } else {
+                    // The toggle is disabled
+                    myWebView.loadUrl("about:blank");
+
+                }
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/openmaker/radiogarden/WelcomeActivity.java
+++ app/src/main/java/com/openmaker/radiogarden/WelcomeActivity.java
@@ -1,9 +1,12 @@
 package com.openmaker.radiogarden;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +14,19 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                //Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+                //Splash.this.startActivity(mainIntent);
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                WelcomeActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+
 
         //
     }
